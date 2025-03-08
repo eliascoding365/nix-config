f15e19a2f7016c6e8caee8c033ec3055a4d1b6b7
Elias Montan <eliasmontan10@gmail.com>
2025-03-08 13:43:37 -0300

    Version 1

---
+++ hosts/common/users/eliasmontan.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  users.users.eliasmontan = {
+    isNormalUser = true;
+    description = "Elias Montan Dev";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+      "video"
+      "input"
+      "plugdev"
+
+    ];
+  
+  packages = [inputs.home-manager.packages.${pkgs.system}.default];
+  };
+
+  home-manager.users.eliasmontan = 
+    import ../../../home/eliasmontan${config.networking.hostName}.nix;
+
+}
--- hosts/eliasmontan-host/configuration.nix
+++ hosts/eliasmontan-host/configuration.nix
@@ -1,22 +1,49 @@
-{ config, pkgs, inputs, lib, ... }:
+  # Edit this configuration file to define what should be installed on
+  # your system.  Help is available in the configuration.nix(5) man page
+  # and in the NixOS manual (accessible by running ‘nixos-help’).
+	{pkgs, ...}: {
+	  imports = [
+	    # Include the results of the hardware scan.
+	    ./hardware-configuration.nix
+	  ];
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 
-{
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      ./main-user.nix
-    ];
+  # Enable networking
+  networking.hostName = "eliasmontan-host"; # Define your hostname.
+  networking.networkmanager.enable = true;
+ 	
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
-  main-user.enable = true;
-  main-user.userName = "eliasmontan";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	
+  # Timezone and localization
+  time.timeZone = "America/Sao_Paulo";
 
-  # Essential system packages
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ALL = "pt_BR.UTF-8";
+  };
+
+  console.keyMap = "br-abnt2";
+ 	
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
     vim
-    vscode
     git
-    foot
+    wget
+    vscode
 
     # Java and build tools
     openjdk21
@@ -26,47 +53,41 @@
     spring-boot-cli
   ];
 
-  # Enable Home Manager for the user
-  home-manager.users.eliasmontan = import ./home.nix;
+# Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # List services that you want to enable:
 
-  # Networking settings
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  # Timezone and localization
-  time.timeZone = "America/Sao_Paulo";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ALL = "pt_BR.UTF-8";
-  };
-
-  # Keyboard layout
-  console.keyMap = "br-abnt2";
-
-  # Sound and multimedia
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  # Enable the OpenSSH daemon.
+  services.openssh = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    settings.PermitRootLogin = "no";
+    allowSFTP = true;
   };
 
-  # Main user settings
-  users.users.eliasmontan = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
   };
 
-  # Allow proprietary packages
-  nixpkgs.config.allowUnfree = true;
-  
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html)
+
 
   # Set system state version
   system.stateVersion = "24.11"; 
